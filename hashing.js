7276afdc5b2ce4444f24c2724058a9748e6e299e
leilaFatec <leila.cruz@fatec.sp.gov.br>
2025-03-07 01:13:24 +0000

    create crypto

---
+++ hashing.js
@@ -0,0 +1,9 @@
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+
+const senha = "minha_senha_super_secreta";
+
+// Hash usando SHA-256 (fixo)
+const sha256Hash = crypto.createHash('sha256').update(senha+"chinforinfula").digest('hex');
+console.log("SHA-256 Hash:", sha256Hash);
+
